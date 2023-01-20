eafdc0fbee39979516d394dcd8b51423bed5aa89
timcooper709 <timcooper709@gmail.com>
2023-01-20 18:00:16 -0500

    change item to title, change string method

--- api/models.py
+++ api/models.py
@@ -4,8 +4,19 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    
     # add additional fields in here
 
     def __str__(self):
         return self.username
+
+class Item(models.Model):
+    product          = models.CharField(max_length=300, null=True, blank=True)      
+    manufacturer     = models.CharField(max_length=300, null=True, blank=True)
+    date_received    = models.DateTimeField(auto_now_add=True)
+    total_quantity   = models.IntegerField(max_length=300, null=True, blank=True)
+    date_shipped     = models.DateTimeField(auto_now_add=True)
+    item_description = models.TextField(null=True, blank=True)
+
+    def __str__ (self):
+        return self.product
