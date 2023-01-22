3a7f9199b9828a1c8d9ba736c492c30a6960de51
timcooper709 <timcooper709@gmail.com>
2023-01-22 11:32:53 -0500

    udateing models

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Aisle, Category
+from .models import User, Aisle, Category, Item
 # Register your models here.
 
 
 admin.site.register(User)
 admin.site.register(Aisle)
 admin.site.register(Category)
+admin.site.register(Item)
--- api/models.py
+++ api/models.py
@@ -11,13 +11,13 @@ class User(AbstractUser):
         return self.username
 
 class Aisle(models.Model):
-    title            = models.IntegerField(max_length=300, null=True, blank=True)
+    title            = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):  
         return self.title  
 
 class Category(models.Model):
-    title     = models.IntegerField(max_length=300, null=True, blank=True)
+    title     = models.CharField(max_length=50, null=True, blank=True)
     aisle     = models.ForeignKey('Aisle', related_name='categories', on_delete=models.CASCADE, null=True,blank=True)
 
 
@@ -29,8 +29,7 @@ class Item(models.Model):
     product          = models.CharField(max_length=300, null=True, blank=True)      
     manufacturer     = models.CharField(max_length=300, null=True, blank=True)
     date_received    = models.DateTimeField(auto_now_add=True)
-    total_quantity   = models.IntegerField(max_length=300, null=True, blank=True)
-    date_shipped     = models.DateTimeField(auto_now_add=True)
+    total_quantity   = models.IntegerField(null=True, blank=True)
     item_description = models.TextField(null=True, blank=True)
 
     def __str__ (self):
